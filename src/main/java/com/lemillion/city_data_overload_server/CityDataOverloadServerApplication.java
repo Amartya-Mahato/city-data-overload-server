f9907779e661ebceece0c7c07022de9e33efa03f
Amartya-Mahato <amartyaipsita135@outlook.com>
2025-07-18 14:10:05 +0530

    first commit

---
+++ src/main/java/com/lemillion/city_data_overload_server/CityDataOverloadServerApplication.java
@@ -0,0 +1,13 @@
+package com.lemillion.city_data_overload_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityDataOverloadServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CityDataOverloadServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lemillion/city_data_overload_server/CityDataOverloadServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lemillion.city_data_overload_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityDataOverloadServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
